88bdbcc76c226c7b8f3da64c234fd2b8bd7d7dd7
Tarakeswararaomakka <makkatarakeswararao3@gmail.com>
2025-10-31 17:12:40 +0530

    Uncomment server setup and add new frontend URL

--- root/server.js
+++ root/server.js
@@ -41,6 +41,7 @@ app.use(
     origin: [
       "http://localhost:8080", // Local frontend (Vite)
       "https://realestate-company.onrender.com", // Render-deployed frontend
+      "realestatesitehyd.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // Allow cookies/auth headers if needed
@@ -65,5 +66,6 @@ app.get("/", (req, res) => {
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
